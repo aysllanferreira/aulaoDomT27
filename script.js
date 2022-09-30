59be9f8f83771dd0f6d20951618e67ad71f52571
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-09-30 01:46:43 -0300

    Esqueleto do projeto

---
+++ script.js
@@ -0,0 +1,35 @@
+// Crie um Titulo em h1 com o texto: Adivinhe a cor e coloque-o dentro da section title.
+
+// Crie dinamicamente 8 circulos que sera no futuro uma paleta de cores.
+// Atribua a classe colourPallete em todos os circulos.
+// Guarde-os na section: pallete.
+
+// Gere cores aleatorias para cada um dos circulos.
+// Crie uma logica para adicionar a classe guess em uma das cores de forma aleatoria
+// A classe guess sera a cor a adivinhar!
+
+// Adicione na section com o ID Result em h1 o RGB da cor a ser adivinhada.
+// Lembre-se que a cor a ser adivinhada possui a classe guess.
+
+// Embaixo do elemento h1 criado no ultimo requisito, crie um elemento h2 dentro de uma div
+// De a essa div a classe score.
+// De o valor do h2 comecando como 0 e coloque-o dentro dessa nova div.
+// A div deve estar dentro do elemento com ID Result.
+// De ao h2 a classe valor
+
+// Crie um EventListener em cada Circulo. DICA: Use QuerySelectorAll ou getElementsByClass.
+// Caso o circulo clicado possuir a classe guess, some 01 no score e atualize as cores.
+// Caso errar, atualize as cores e zere a pontuacao.
+// Crie uma variavel global que receba o valor 0.
+// Crie uma funcao somente para remover as cores e adicionar novas.
+
+// Agora estilize, sinta-se livre pra melhorar o CSS!!!
+// Nao precisa usar DOM.
+
+// Divirta-se !!!
+
+// Variaveis globais
+
+// Funcoes
+
+// Chamada das funcoes
